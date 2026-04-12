dafc6a6d9ef437212039ce2a32301cd7ca426541
Parth Mangtani <impmmmm@gmail.com>
2026-04-12 08:00:50 +0530

    feat: initialize Django project settings with PostgreSQL, Allauth, and Whitenoise configuration

--- wait/settings.py
+++ wait/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = "wait.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
+        "NAME": config("DB_NAME", default="postgres"),
+        "USER": config("DB_USER", default="postgres"),
+        "PASSWORD": config("DB_PASSWORD", default=""),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
     }
 }
 
